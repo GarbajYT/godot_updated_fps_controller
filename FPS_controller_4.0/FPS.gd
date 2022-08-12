8d3f2643b6183d00ba3a1010c42b27d4d24ed1c0
PinkTurkey0 <95888762+PinkTurkey0@users.noreply.github.com>
2022-08-12 01:34:35 -0500

    Updated Godot 4.0 code for Godot 4 Alpha 13
    FPS.tscn - Changed default rotation to 0 so the player is upright
    
    FPS.gd - Fix interpolation, use the projects gravity setting, update input direction code, move jumping code

--- FPS_controller_4.0/FPS.gd
+++ FPS_controller_4.0/FPS.gd
@@ -1,20 +1,16 @@
-extends KinematicBody
+extends CharacterBody3D
 
 var speed = 7
 const ACCEL_DEFAULT = 7
 const ACCEL_AIR = 1
 @onready var accel = ACCEL_DEFAULT
-var gravity = 9.8
+@onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var jump = 5
 
 var cam_accel = 40
 var mouse_sense = 0.1
-var snap
 
-var direction = Vector3()
-var velocity = Vector3()
 var gravity_vec = Vector3()
-var movement = Vector3()
 
 @onready var head = $Head
 @onready var camera = $Head/Camera
@@ -32,42 +28,32 @@ func _input(event):
 
 func _process(delta):
 	#camera physics interpolation to reduce physics jitter on high refresh-rate monitors
-	if Engine.get_frames_per_second() > Engine.iterations_per_second:
-		camera.set_as_toplevel(true)
-		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(head.global_transform.origin, cam_accel * delta)
+	if Engine.get_frames_per_second() > Engine.physics_ticks_per_second:
+		camera.top_level = true
+		camera.global_transform.origin = camera.global_transform.origin.lerp(head.global_transform.origin, cam_accel * delta)
 		camera.rotation.y = rotation.y
 		camera.rotation.x = head.rotation.x
 	else:
-		camera.set_as_toplevel(false)
+		camera.top_level = false
 		camera.global_transform = head.global_transform
 		
 func _physics_process(delta):
 	#get keyboard input
-	direction = Vector3.ZERO
-	var h_rot = global_transform.basis.get_euler().y
-	var f_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	var h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direction = (transform.basis * Vector3(direction.x, 0, direction.y)).normalized()
 	
 	#jumping and gravity
 	if is_on_floor():
-		snap = -get_floor_normal()
 		accel = ACCEL_DEFAULT
 		gravity_vec = Vector3.ZERO
+		if Input.is_action_just_pressed("ui_accept"):
+			gravity_vec = Vector3.UP * jump
 	else:
-		snap = Vector3.DOWN
 		accel = ACCEL_AIR
 		gravity_vec += Vector3.DOWN * gravity * delta
-		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		snap = Vector3.ZERO
-		gravity_vec = Vector3.UP * jump
 	
 	#make it move
-	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
-	movement = velocity + gravity_vec
+	velocity = velocity.lerp(direction * speed, accel * delta)
+	velocity += gravity_vec
 	
-	move_and_slide_with_snap(movement, snap, Vector3.UP)
-	
-	
-	
+	move_and_slide()
