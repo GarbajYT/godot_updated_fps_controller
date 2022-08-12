b630caee297d73eea46778e75c8d8701cf296fd0
PinkTurkey0 <95888762+PinkTurkey0@users.noreply.github.com>
2022-08-12 01:45:37 -0500

    Updated Godot 4.0 code for Godot 4 Alpha 13
    TPS.tscn - Changed default rotation to 0 so the player is upright
    
    TPS.gd - Fix interpolation, use the projects gravity setting, update input direction code, move jumping code

--- TPS_controller_4.0/TPS.gd
+++ TPS_controller_4.0/TPS.gd
@@ -1,33 +1,31 @@
-extends KinematicBody
+extends CharacterBody3D
 
 var speed = 7
 const ACCEL_DEFAULT = 10
 const ACCEL_AIR = 1
 @onready var accel = ACCEL_DEFAULT
-var gravity = 9.8
+@onready var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var jump = 5
 
 var cam_accel = 40
 var mouse_sense = 0.1
-var snap
 
 var angular_velocity = 30
 
-var direction = Vector3()
-var velocity = Vector3()
+var direction = Vector3.ZERO
 var gravity_vec = Vector3()
-var movement = Vector3()
 
 @onready var head = $Head
 @onready var campivot = $Head/CamPivot
 @onready var mesh = $MeshInstance
+
 
 func _ready():
 	#hides the cursor
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 	#mesh no longer inherits rotation of parent, allowing it to rotate freely
-	mesh.set_as_toplevel(true)
+	mesh.top_level = true
 	
 func _input(event):
 	#get mouse input for camera rotation
@@ -38,15 +36,14 @@ func _input(event):
 
 func _process(delta):
 	#physics interpolation to reduce jitter on high refresh-rate monitors
-	var fps = Engine.get_frames_per_second()
-	if fps > Engine.iterations_per_second:
-		campivot.set_as_toplevel(true)
-		campivot.global_transform.origin = campivot.global_transform.origin.linear_interpolate(head.global_transform.origin, cam_accel * delta)
+	if Engine.get_frames_per_second() > Engine.physics_ticks_per_second:
+		campivot.top_level = true
+		campivot.global_transform.origin = campivot.global_transform.origin.lerp(head.global_transform.origin, cam_accel * delta)
 		campivot.rotation.y = rotation.y
 		campivot.rotation.x = head.rotation.x
-		mesh.global_transform.origin = mesh.global_transform.origin.linear_interpolate(global_transform.origin, cam_accel * delta)
+		mesh.global_transform.origin = mesh.global_transform.origin.lerp(global_transform.origin, cam_accel * delta)
 	else:
-		campivot.set_as_toplevel(false)
+		campivot.top_level = false
 		campivot.global_transform = head.global_transform
 		mesh.global_transform.origin = global_transform.origin
 
@@ -56,31 +53,21 @@ func _process(delta):
 
 func _physics_process(delta):
 	#get keyboard input
-	direction = Vector3.ZERO
-	var h_rot = global_transform.basis.get_euler().y
-	var f_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
-	var h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	direction = (transform.basis * Vector3(direction.x, 0, direction.y)).normalized()
 	
 	#jumping and gravity
 	if is_on_floor():
-		snap = -get_floor_normal()
 		accel = ACCEL_DEFAULT
 		gravity_vec = Vector3.ZERO
+		if Input.is_action_just_pressed("ui_accept"):
+			gravity_vec = Vector3.UP * jump
 	else:
-		snap = Vector3.DOWN
 		accel = ACCEL_AIR
 		gravity_vec += Vector3.DOWN * gravity * delta
-		
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		snap = Vector3.ZERO
-		gravity_vec = Vector3.UP * jump
 	
 	#make it move
-	velocity = velocity.linear_interpolate(direction * speed, accel * delta)
-	movement = velocity + gravity_vec
+	velocity = velocity.lerp(direction * speed, accel * delta)
+	velocity += gravity_vec
 	
-	move_and_slide_with_snap(movement, snap, Vector3.UP)
-	
-	
-	
+	move_and_slide()
