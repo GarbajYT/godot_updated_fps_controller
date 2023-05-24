6c471c04bd043f55eadb71881a1f67ddb59895ac
Hasan Sabri <84398988+gooficat@users.noreply.github.com>
2023-05-24 10:35:43 +0100

    Update TPS.gd
    fixed jump

--- TPS_controller_4.0/TPS.gd
+++ TPS_controller_4.0/TPS.gd
@@ -75,6 +75,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		snap = Vector3.ZERO
 		gravity_vec = Vector3.UP * jump
+	velocity.y = 0
 	
 	#make it move
 	movement = movement.lerp(direction * speed, accel * delta)
